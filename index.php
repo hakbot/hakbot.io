ba3bc633e69f00af133b72ff96ed7e5a4bf37a77
Steve Springett <steve@item-a10907.wks.axway.int>
2016-05-12 12:32:02 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include ("security-headers.php") ?>
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+  <title>Hakbot: Security Tool Automation for the Secure Software Development Lifecycle</title>
+</head>
+<body>
+
+<div id="wrapper" style="width:100%; text-align:center">
+  <img src="images/hakbot-logo-250x250.png"/>
+  <p>Launching soon...</p>
+</div>
+
+</body>
+</html>
